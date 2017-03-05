e4e7658d032849ade609d609929ecc52783a1593
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-03-05 22:08:59 +0100

    starting async

--- generator.js
+++ generator.js
@@ -3,27 +3,25 @@ var hget=require('hget');
 var marked=require('marked');
 var Term=require('marked-terminal');
 
-function getRandomPonyFooArticle(gen){
-    var g=gen();
-    request('https://ponyfoo.com/articles/random',(err,res,body) => {
-        if(err){
-            g.throw(err);return;
+function getRandomPonyFooArticle (gen) {
+    var g = gen();
+    request('https://ponyfoo.com/articles/random', (err, res, body) => {
+        if (err) {
+            g.throw(err); return;
         }
-        g.next(body)
+        g.next(body);
     });
 }
 
-getRandomPonyFooArticle(function* printRandomArticle(){
-    var html=yield;
-    console.log('a');
-    var md=hget(html, {
-        markdown:true,
-        root:'main',
-        ignore:'.at-subscribe,.mm-comments,.de-sidebar'
+getRandomPonyFooArticle(function* printRandomArticle () {
+    var html = yield;
+    var md = hget(html, {
+        markdown: true,
+        root: 'main',
+        ignore: '.at-subscribe,.mm-comments,.de-sidebar'
     });
-    console.log(md);
-    var txt= marked(md,{
+    var txt = marked(md, {
         renderer: new Term()
     });
     console.log(txt);
-})
+});
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 //require('./promise');
 require('./generator');
+require('./async');
